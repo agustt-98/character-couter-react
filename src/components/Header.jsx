a866063ebe66c6abe697f9812219c483fe7f56ef
agustt-98 <agustinsimone.juan23@gmail.com>
2026-08-06 08:43:43 -0300

    add: header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return <header>
+        <div>
+            <p>C</p>
+            <h1>Character Counter</h1>
+        </div>
+        <button>☀</button>
+    </header>
+}
+
+export { Header } 
